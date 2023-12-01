f939422727b5121bfb55437c2daddac42b3dc9d6
gokulgokul8351 <gokulgokul8351@gmail.com>
2023-12-01 13:01:08 +0530

    4th changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
 import Login from './components/Login'
 
 function App() {
   return (
-    <>
-      <Home />
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={<Home />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
